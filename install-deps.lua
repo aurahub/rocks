d1b6151fa3c4efbbdc11cfb53ca39f13f47e7636
lizongti <lzongti@gmail.com>
2019-04-11 10:58:05 +0800

    install-deps

--- install-deps.lua
+++ install-deps.lua
@@ -1,27 +1,6 @@
-local rocks_deps = {
-    "luasocket",
-    "lua-protobuf",
-    "luafilesystem",
-    "rapidjson",
-    "deque",
-    "luacrypto", -- luarocks install luacrypto OPENSSL_DIR=/usr/local/Cellar/openssl/1.0.2r/; --apt-get install libssl-dev
-    "openssl",
-    "luajwt",
-    "lbase64"
-}
-
 local aura_deps = {
     "spack"
 }
-
-local _lit_deps = {
-    "cyrilis/luvit-mongodb" -- no need to install, already in project
-}
-
-for _, libname in pairs(rocks_deps) do
-    os.execute("luarocks install " .. libname)
-    print("luarocks install " .. libname .. " ok.")
-end
 
 for _, libname in pairs(aura_deps) do
     os.execute("git clone https://github.com/aurahub/" .. libname .. ".git")
@@ -29,31 +8,41 @@ for _, libname in pairs(aura_deps) do
     print("aurahub install " .. libname .. " ok.")
 end
 
-for _, libname in pairs(lit_deps) do
-    os.execute("lit install " .. libname)
-    print("lit install " .. libname .. " ok.")
-end
-
 --[[
     apt-get upgrade & apt-get update
     apt-get install build-essential
-    apt-get install luajit-5.1-dev
-    apt-get install luvit
-    apt-get install luarocks
-    apt-get install libssl1.0-dev
+    apt-get install cmake
     apt-get install vim
     apt-get install git
     apt-get install curl
+    apt-get install luarocks
+
+
+
+    apt-get install libssl1.0-dev
     apt-get install libjemalloc-dev
     apt-get install libboost-all-dev
+    apt-get install libghc-pcre-light-dev
+    apt-get install zlib1g-dev
+
+    luarocks install luv
+    luarocks install lua-cjson
+    luarocks install lua-zlib
     luarocks install luasocket
     luarocks install lua-protobuf
     luarocks install luafilesystem
     luarocks install rapidjson
+    luarocks install luacrypto
     luarocks install deque
     luarocks install lbase64
-    luarocks install 
+    luarocks install lrexlib-pcre
     luarocks install luajwt
-    cd /tmp/;curl -L https://github.com/luvit/lit/raw/master/get-lit.sh | sh; cp * /usr/local/bin/;rm -rf /tmp/*
-    cd /tmp/;git clone https://github.com/aurahub/spack.git;cd spack;mdir build;cd build;cmake ..;make install;rm -rf /tmp/*
+    luarocks install openssl
+    luarocks install luazlib
+  
+    wget http://luajit.org/download/LuaJIT-2.0.5.zip
+    git clone https://github.com/aurahub/spack.git;
+
+docker stop rocks;docker rm rocks;docker run -d -p 10000:10000 -p 10080:10080 --name rocks --hostname rocks -v /C/Users/Administrator/Documents/data:/data --link 357c133ae4a4:mongo -w /data/server/ rocks luajit logic/server.lua
+docker stop rocks;docker rm rocks;docker run -it -p 10000:10000 -p 10080:10080 --name rocks --hostname rocks -v /C/Users/Administrator/Documents/data:/data --link 357c133ae4a4:mongo rocks /bin/bash
 ]]
